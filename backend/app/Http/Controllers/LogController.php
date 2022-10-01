7ce43696298564fb88368c77cf1baf891fccaeb7
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2022-10-01 21:08:34 +0400

    add device location,worker number, remove device id

--- backend/app/Http/Controllers/LogController.php
+++ backend/app/Http/Controllers/LogController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Log;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log as CustomLog;
+
 
 class LogController extends Controller
 {
@@ -14,13 +16,12 @@ class LogController extends Controller
      */
     public function index(Request $request)
     {
-        return Log::paginate($request->per_page ?? 10);
-    }
-
-    public function range(Request $request)
-    {
         $model = Log::query();
-        $model->whereBetween("log_date", [$request->from, $request->to]);
+
+        $model->when($request->from && $request->to, function ($q) use ($request) {
+            $q->whereBetween("log_date", [$request->from, $request->to]);
+        });
+
         $model->when($request->user_id, function ($q) use ($request) {
             $q->where("user_id", $request->user_id);
         });
@@ -35,9 +36,57 @@ class LogController extends Controller
         $model->when($request->user_id, function ($q) use ($request) {
             $q->where("user_id", $request->user_id);
         });
-        return $model->get();
+        return $model->get(["user_id","log_date","log_time","type"]);
     }
 
+    public function getLastSerialIdFromDb()
+    {
+        return Log::orderByDesc("id")->pluck("serial_no")[0] ?? 0;
+    }
+
+    public function sync_from_mdb()
+    {
+        return Log::insert($this->mdb_log());
+    }
+
+
+    public function mdb_log()
+    {
+        $MASQL_DB_PATH = env("MASQL_DB_PATH");
+
+        try {
+            $db = new \PDO("odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=$MASQL_DB_PATH;");
+
+            $lastRecord = $this->getLastSerialIdFromDb();
+            $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $stmt = $db->prepare("select cr.PCode, cr.DataTime, cr.EquptID,cr.EquptName, cr.RecordSerialNumber, Equipment.EquptSN
+            from CardRecord as cr INNER JOIN Equipment ON cr.EquptID=Equipment.EquptID where RecordSerialNumber > $lastRecord and cr.PersonnelID > 0  ORDER BY RecordSerialNumber asc");
+            $stmt->execute();
+
+            $stmt->setFetchMode(\PDO::FETCH_ASSOC);
+            $records = $stmt->fetchAll();
+
+            $arr = [];
+
+            foreach ($records as $record) {
+                $datetime = $this->convert_date($record["DataTime"]);
+
+                $arr[] = [
+                    "serial_no" => $record["RecordSerialNumber"],
+                    "user_id" => $record["PCode"],
+                    "type" => $record["EquptID"] == 1 ? "C/in" : "C/out",
+                    "device_id" => $record["EquptName"],
+                    "device_model" => substr($record["EquptSN"], 0, 7),
+                    "log_date" => $datetime[0] ?? "",
+                    "log_time" => explode(".", $datetime[1])[0] ?? "",
+                ];
+            }
+            return ($arr);
+        } catch (\PDOException $e) {
+            CustomLog::channel("custom")->error($e->getMessage());
+            return $e->getMessage();
+        }
+    }
     /**
      * Store a newly created resource in storage.
      *
@@ -46,7 +95,7 @@ class LogController extends Controller
      */
     public function store(Request $request)
     {
-        $file = base_path() . "/logs/OXSAI_timedata_DX.csv";
+        $file = base_path() . "/logs/logs.csv";
 
         if (!file_exists($file)) {
             return [
@@ -57,33 +106,68 @@ class LogController extends Controller
 
         $first = true;
         $data = [];
+        $punch = "";
 
-        if (($handle = fopen(base_path() . "/logs/OXSAI_timedata_DX.csv", 'r')) !== false) {
+        if (($handle = fopen(base_path() . "/logs/logs.csv", 'r')) !== false) {
             while (($row = fgetcsv($handle, 1000, ',')) !== false) {
 
                 if ($first) {
                     $first = false;
                 } else {
-                    $datetime = explode(" ", $row[1]);
-                    $data[] = [
+                    $datetime = explode(" ", $row[2]);
+
+                    $arr = [
                         "user_id" => $row[0],
                         "log_date" => date("Y-m-d", strtotime($datetime[0])),
                         "log_time" => $datetime[1],
-                        "device_id" => $row[2],
+                        "device_id" => $row[1],
                         "device_model" => substr($row[2], 0, 7),
                         "serial_no" => $row[3],
-                        "type" => $row[2] == "OX-8662021010065" ? "C/in" : "C/out"
                     ];
+
+                    if ($row[1] == env("DEVICE_IN") || $row[1] == env("DEVICE_OUT")) {
+                        if ($row[1] == env("DEVICE_IN")) {
+                            $arr["type"] = "C/in";
+                        } else if ($row[1] == env("DEVICE_OUT")) {
+                            $arr["type"] = "C/out";
+                        }
+
+                        $data[] = $arr;
+                    }
                 }
             }
             fclose($handle);
         }
         try {
             $created = Log::insert($data);
-            // $created ? unlink(base_path() . "/logs/OXSAI_timedata_DX.csv") : 0;
+            $created ? unlink(base_path() . "/logs/logs.csv") : 0;
             return $created ?? 0;
         } catch (\Throwable $th) {
             throw $th;
         }
+    }
+
+    public function convert_date($oaDate)
+    {
+
+        $days = floor($oaDate);
+        $msecsFloat = ($oaDate - $days) * 86400000;
+        $msecs = floor($msecsFloat);
+        $hours = floor($msecs / 3600000);
+        $msecs %= 3600000;
+        $mins = floor($msecs / 60000);
+        $msecs %= 60000;
+        $secs = floor($msecs / 1000);
+        $msecs %= 1000;
+
+        $baseDate = new \DateTimeImmutable('1899-12-30 00:00:00');
+
+        $date = $baseDate->add(new \DateInterval(sprintf('P%sDT%sH%sM%sS', $days, $hours, $mins, $secs)))->format('Y-m-d H:i:s');
+
+        $final_date = (array) new \DateTime($date);
+
+        $datetime = explode(" ", $final_date["date"]);
+
+        return $datetime;
     }
 }
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -164,11 +164,21 @@ export default {
     Model: "Report",
     total: 0,
     headers: [
-      { text: "User Id", align: "left", sortable: false, value: "user_id" },
+      {
+        text: "Worker Number",
+        align: "left",
+        sortable: false,
+        value: "user_id",
+      },
       { text: "Log Date", align: "left", sortable: false, value: "log_date" },
       { text: "Log Time", align: "left", sortable: false, value: "log_time" },
       { text: "Type", align: "left", sortable: false, value: "type" },
-      { text: "Device Id", align: "left", sortable: false, value: "device_id" },
+      {
+        text: "Device Location",
+        align: "left",
+        sortable: false,
+        value: "device_id",
+      },
     ],
     options: {},
     columns: {},
@@ -207,7 +217,7 @@ export default {
   },
 
   methods: {
-    getDataFromApi(url = `http://127.0.0.1:8000/api/log`) {
+    getDataFromApi(url = `/log`) {
       const { page, itemsPerPage } = this.options;
 
       let options = {
@@ -224,23 +234,20 @@ export default {
     },
 
     json_to_csv(json) {
-      let data = json.map((e) => ({
-        "User Id": e.user_id,
-        "Log Date": e.log_date,
-        "Log Time": e.log_time,
-        Type: e.type,
-        "Device Id": e.device_id,
-      }));
-
-      let header = Object.keys(data[0]).join(",") + "\n";
-      let rows = "";
-      data.forEach((e) => {
-        rows += Object.values(e).join(",").trim() + "\n";
+      let rows = json.map((e, i) => Object.values(e));
+      let str = "";
+      rows.forEach((e) => {
+        str += e.replace(",", " ") + "\n";
       });
-      return header + rows;
+
+      return str;
     },
     sync_record() {
-      this.$axios.post(`http://127.0.0.1:8000/api/sync`).then(({ data }) => {
+      this.$axios.post(`/sync_from_mdb`).then(({ data }) => {
+        if (!data.status && data.status !== undefined) {
+          alert(data.message);
+          return;
+        }
         this.getDataFromApi();
       });
     },
@@ -252,12 +259,10 @@ export default {
           user_id: this.payload.user_id,
         },
       };
-      this.$axios
-        .get(`http://127.0.0.1:8000/api/range`, payload)
-        .then(({ data }) => {
-          this.data = data.data;
-          this.total = data.total;
-        });
+      this.$axios.get(`/log`, payload).then(({ data }) => {
+        this.data = data.data;
+        this.total = data.total;
+      });
     },
 
     export_record() {
@@ -268,25 +273,24 @@ export default {
           user_id: this.payload.user_id,
         },
       };
-      this.$axios
-        .get(`http://127.0.0.1:8000/api/export`, payload)
-        .then(({ data }) => {
-          if (data.length == 0) {
-            this.snackbar = true;
-            this.response = "No record to download";
-            return;
-          }
-          let csvData = this.json_to_csv(data);
-          let a = document.createElement("a");
-          a.setAttribute(
-            "href",
-            "data:text/plain;charset=utf-8, " + encodeURIComponent(csvData)
-          );
-          a.setAttribute("download", "logs.txt");
-          document.body.appendChild(a);
-          a.click();
-          document.body.removeChild(a);
-        });
+      this.$axios.get(`/export`, payload).then(({ data }) => {
+        if (data.length == 0) {
+          this.snackbar = true;
+          this.response = "No record to download";
+          return;
+        }
+        let csvData = this.json_to_csv(data);
+
+        let a = document.createElement("a");
+        a.setAttribute(
+          "href",
+          "data:text/plain;charset=utf-8, " + encodeURIComponent(csvData)
+        );
+        a.setAttribute("download", "logs.txt");
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      });
     },
   },
 };
